ffd5375cdccadf3ff35010e6d0fa16e91856cab4
Trevor <marketing@southernoaksinnstaug.com>
2020-06-12 11:17:49 -0400

    updated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <Router>
+export default class App extends Component {
+  render() {
+    return (
       <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route path="*" component={NoMatch} />
-        </Switch>
+        <h1>Home Page</h1>
       </div>
-    </Router>
-  );
+    );
+  }
 }
-
-export default App;
